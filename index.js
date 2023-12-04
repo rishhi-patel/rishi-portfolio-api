c7a57d8ee4a47638c8190d1a02abc15e4b94eab1
Rishi <developer.rishi.99@gmail.com>
2023-12-05 00:21:48 +0530

    Added : SS

--- index.js
+++ index.js
@@ -29,21 +29,49 @@ async function captureScreenshot(url, delay = 5000) {
 // POST endpoint to receive an array of URLs and return their screenshots
 app.get("/", async (req, res) => {
   try {
-    const urls = ["https://espacecarre.com"]
+    const urls = [
+      {
+        name: "Eespacecarre- Ecommerce",
+        url: "https://stage-e-commerce.netlify.app",
+        des: "A user-friendly e-commerce app designed for seamless shopping experiences. It features a sleek, intuitive interface with advanced search capabilities, personalized recommendations, and secure checkout options",
+      },
+      {
+        name: "Eespacecarre- Admin",
+        url: "https://stage-admin-e-commerce.netlify.app",
+        des: "A comprehensive e-commerce admin panel offering real-time analytics, inventory management, and order tracking. Equipped with user-friendly dashboards for efficient site management and customer support tools.",
+      },
+      {
+        name: "",
+        url: "https://geometra.rukkor.io/login",
+        des: "",
+      },
+      {
+        name: "",
+        url: "https://admin-thefootballagency.netlify.app",
+        des: "Created a specialized construction industry tool for managing 2D/3D drawings and BIM models, complete with a customizable cost estimation framework.",
+      },
+      {
+        name: "Geometa",
+        url: "https://stage-admin-jenny-point.netlify.app",
+        des: "",
+      },
+      {
+        name: "Scoutible",
+        url: "https://www.scoutible.com",
+        des: "Developed a game-based hiring platform using mobile gaming to accurately identify ideal candidate fits.",
+      },
+    ]
     const results = []
 
     for (const url of urls) {
-      const tempScreenshotPath = await captureScreenshot(url) // Delay is used here
+      const tempScreenshotPath = await captureScreenshot(url.url) // Delay is used here
 
       // Read the screenshot file and send it as a base64 encoded string
       const fileContent = fs.readFileSync(tempScreenshotPath, {
         encoding: "base64",
       })
-      results.push({
-        url,
-        screenshotBase64: `data:image/png;base64,${fileContent}`,
-      })
-
+      url.image = `data:image/png;base64,${fileContent}`
+      results.push(url)
       // Delete the temporary file
       fs.unlinkSync(tempScreenshotPath)
     }
